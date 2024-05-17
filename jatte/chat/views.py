5b18dbf79d8d43fb01110a669d083fc5ff9ff80c
OllyYa <exrhodo@gmail.com>
2024-05-17 15:07:43 +0300

    page for listing out rooms

--- jatte/chat/views.py
+++ jatte/chat/views.py
@@ -1,9 +1,15 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.shortcuts import render
 from django.views.decorators.http import require_POST
 
+from account.models import User
+
 from .models import Room
+
+
 
 
 @require_POST
@@ -14,3 +20,11 @@ def create_room(request, uuid):
     Room.objects.create(uuid=uuid, client=name, url=url)
 
     return JsonResponse({'message': 'room created'})
+
+
+@login_required
+def admin(request):
+    rooms = Room.objects.all()
+    users = User.objects.filter(is_staff=True)
+
+    return render(request, 'chat/admin.html', {'rooms': rooms, 'users': users})
